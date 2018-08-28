ebe78bd1be00dab179d85622c82e2ad2cbc85fbe
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-28 17:24:24 +0900

    switch

--- src/reducers/SelectionReducer.js
+++ src/reducers/SelectionReducer.js
@@ -1,4 +1,8 @@
 export default (state, action) => {
-  console.log(action);
-  return null;
+  switch (action.type) {
+    case 'select_libarary':
+      return action.payload;
+    default:
+      return state;
+  }
 };
